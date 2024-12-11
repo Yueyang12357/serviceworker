1b082b77da1e347cc7e9ecfc16d446952bb777f5
e <yoshi12357@live.com>
2024-12-12 02:48:26 +0800

    feat: 制作PPT，增加sync样例

--- server/app.js
+++ server/app.js
@@ -2,12 +2,6 @@ const express = require("express");
 const fs = require("fs");
 const webpush = require("web-push");
 
-const vapidKeys = {
-  publicKey:
-    "BDNofQmRYyHU9MQWpgib4rDnN8SVKE_hp6gyb7RZlYBFtz2Mi-I3QQ8emDgO4bvnoxEz1JEda08rdmp2P0NuMNE",
-  privateKey: "xCMCi_15SEp5GENil-tcK9zCjhFjN7aEiQJSnuMz5dw",
-};
-
 const app = express();
 // 私钥跟证书
 const httpsOption = {
@@ -19,18 +13,25 @@ const https = require("https").Server(httpsOption, app);
 
 const port = 3000;
 
-// 这个是需要在 google cloud platform 中创建的项目id
-webpush.setGCMAPIKey("GCM API Key");
+// service worker 消息推送
+const vapidKeys = {
+  publicKey:
+    "BDNofQmRYyHU9MQWpgib4rDnN8SVKE_hp6gyb7RZlYBFtz2Mi-I3QQ8emDgO4bvnoxEz1JEda08rdmp2P0NuMNE",
+  privateKey: "xCMCi_15SEp5GENil-tcK9zCjhFjN7aEiQJSnuMz5dw",
+};
+
+// 这个是需要在 google cloud platform 中创建的项目GCMApiKey
+webpush.setGCMAPIKey("GCMApiKey");
 
 webpush.setVapidDetails(
-  // 邮箱
   "yoshi12357@163.com",
   vapidKeys.publicKey,
   vapidKeys.privateKey
 );
 
+// 5. 服务器通过`subscription`对象推送消息
 app.get("/push", (req, res) => {
-  // 这里的 pushSubscription 就是上面注册成功后返回的 subscription 对象
+  // 注册成功后返回的 subscription 对象
   const pushSubscription = {
     endpoint:
       "https://fcm.googleapis.com/fcm/send/cSAH1Q7Fa6s:APA91bEgYeKNXMSO1rcGAOPzt3L9fMhyjL-zSPV5JfiKwgqtbx_Q4de_8plEY_QViLnhfe6-0fUgdo7Z3Gqpml3zIBSfO6IISDYdF9kzL2h_dbZ_FE_YKbKOG70gMG_A74xwK1vsocCv",
@@ -41,6 +42,7 @@ app.get("/push", (req, res) => {
     },
   };
 
+  // 通过加密后发送到客户端
   webpush.sendNotification(pushSubscription, "Hello world").then((result) => {
     res.send(result);
   });
--- web/service-worker.js
+++ web/service-worker.js
@@ -22,6 +22,7 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+// 离线优先Cache First
 self.addEventListener("fetch", (event) => {
   console.log("拦截fetch:", event.request);
   event.respondWith(
@@ -48,6 +49,16 @@ self.addEventListener("fetch", (event) => {
         });
       }
     })
+    // 综合策略-更新后使用
+    // caches.open("demo-cache").then((cache) => {
+    //   return cache.match(event.request).then((response) => {
+    //     const fetchPromise = fetch(event.request).then((networkResponse) => {
+    //       cache.put(event.request, networkResponse.clone());
+    //       return networkResponse;
+    //     });
+    //     return response || fetchPromise;
+    //   });
+    // })
   );
 });
 
@@ -59,8 +70,41 @@ self.addEventListener("message", (event) => {
   }, 2000);
 });
 
-// 注册 push 事件
+// 监听 sync 事件
+self.addEventListener("sync", (event) => {
+  console.log("sync", event);
+  if (event.tag === "sync") {
+    event.waitUntil(
+      // 同步数据
+      syncData()
+    );
+  }
+});
+
+// 注册 push 事件，监听并推送消息
 self.addEventListener("push", (event) => {
   const data = event.data.json();
+  // 7. `ServiceWorker`通过`showNotification`方法，显示消息
   self.registration.showNotification(data.title);
 });
+
+// 离线推送
+self.addEventListener("notificationcallback", (event) => {
+  const data = event.data.json();
+  // 7. `ServiceWorker`通过`showNotification`方法，显示消息
+  self.registration.showNotification(data.title);
+});
+
+const syncData = () => {
+  fetch("./data.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log("update-caches");
+      // 更新缓存
+      caches.open("demo-cache").then((cache) => {
+        cache.put("/data.json", new Response(JSON.stringify(data)));
+      });
+    });
+};
